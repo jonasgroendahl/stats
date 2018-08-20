13619c98edcf1c4d0ea72c479ee963da841f150c
jonas <jonas.grondahl@wexer.com>
2018-08-20 11:23:49 +0200

    changes

--- src/StatsPage.js
+++ src/StatsPage.js
@@ -11,12 +11,12 @@ import {
   Select,
   MenuItem,
   Button,
-  Paper,
   TextField,
   Popper,
   Card,
   CardContent
 } from "@material-ui/core";
+import axios from "axios";
 
 class App extends Component {
   state = {
@@ -25,30 +25,52 @@ class App extends Component {
         name:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, nulla.",
         views: 44,
-        provider_name: "Les Mills",
-        date: '2018-05-05 16:00:00',
-        type: 'Scheduled'
+        date: "2018-05-05 16:00:00",
+        type: "Scheduled",
+        provider_name: "Club Virtual Bike",
+        category: "Cycling",
+        level: "Beginner",
+        avg: 20,
+        count: 22
       }
     ],
-    report: 'class_report',
+    report: "class_report",
     interval: "1 week",
     type: "All",
-    show: 'All',
+    show: "All",
     customDateEl: null,
-    start_date: '',
-    end_date: ''
+    start_date: "",
+    end_date: "",
+    gymId: 124
   };
+
+  async componentDidMount() {
+    const result = await axios.get(
+      `http://localhost:3001/v2/stats?summed=1&gym_id=${this.state.gymId}`
+    );
+    console.log(result);
+    this.setState({ data: result.data });
+  }
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  toggleDatepicker = (event) => {
+  toggleDatepicker = event => {
     this.setState({ customDateEl: event.target });
-  }
+  };
 
   render() {
-    const { data, interval, type, show, report, start_date, end_date, customDateEl } = this.state;
+    const {
+      data,
+      interval,
+      type,
+      show,
+      report,
+      start_date,
+      end_date,
+      customDateEl
+    } = this.state;
     return (
       <div className="App stats-container">
         <Grid container style={{ marginBottom: 12 }}>
@@ -56,9 +78,17 @@ class App extends Component {
             <FormLabel component="legend">Select Report</FormLabel>
           </Grid>
           <Grid item xs={6}>
-            <Select fullWidth value={report} onChange={this.handleChange} name="report">
+            <Select
+              fullWidth
+              value={report}
+              onChange={this.handleChange}
+              name="report"
+            >
               <MenuItem value="class_report">Class title report</MenuItem>
-              <MenuItem value="calendar_report">Wexer Count calendar report</MenuItem>
+              <MenuItem value="schedule_report">Schedule report</MenuItem>
+              <MenuItem value="calendar_report">
+                Wexer Count calendar report
+              </MenuItem>
             </Select>
           </Grid>
         </Grid>
@@ -90,7 +120,11 @@ class App extends Component {
               />
               <FormControlLabel
                 control={<Radio onClick={this.toggleDatepicker} />}
-                label={customDateEl || (start_date !== '' && end_date !== '') ? `${start_date} - ${end_date}` : 'Custom'}
+                label={
+                  customDateEl || (start_date !== "" && end_date !== "")
+                    ? `${start_date} - ${end_date}`
+                    : "Custom"
+                }
                 value="custom"
               />
             </RadioGroup>
@@ -107,21 +141,13 @@ class App extends Component {
               name="type"
               value={type}
             >
-              <FormControlLabel
-                value="All"
-                control={<Radio />}
-                label="All"
-              />
+              <FormControlLabel value="All" control={<Radio />} label="All" />
               <FormControlLabel
                 value="Scheduled"
                 control={<Radio />}
                 label="Scheduled"
               />
-              <FormControlLabel
-                value="Live"
-                control={<Radio />}
-                label="Live"
-              />
+              <FormControlLabel value="Live" control={<Radio />} label="Live" />
               <FormControlLabel
                 value="On Demand"
                 control={<Radio />}
@@ -141,11 +167,7 @@ class App extends Component {
               name="show"
               value={show}
             >
-              <FormControlLabel
-                value="All"
-                control={<Radio />}
-                label="All"
-              />
+              <FormControlLabel value="All" control={<Radio />} label="All" />
               <FormControlLabel
                 value="Only Count enabled"
                 control={<Radio />}
@@ -155,16 +177,44 @@ class App extends Component {
           </Grid>
         </Grid>
         <Button variant="raised">Generate report</Button>
-        {report == 'class_report' &&
-          <StatsTable data={data} />}
-        {report == 'calendar_report' && <Calendar />}
+        {report !== "calendar_report" ? (
+          <StatsTable data={data} report={report} />
+        ) : (
+          <Calendar />
+        )}
         <Popper open={Boolean(customDateEl)} anchorEl={customDateEl}>
           <Card>
             <CardContent>
-              <TextField type="date" name="start_date" onChange={this.handleChange} style={{ marginRight: 10 }} />
-              <TextField type="date" name="end_date" onChange={this.handleChange} />
-              <Button onClick={() => this.setState({ customDateEl: null, interval: '1 week', start_date: '', end_date: '' })}>Cancel</Button>
-              <Button disabled={!start_date || !end_date} variant="outlined" onClick={() => this.setState({ customDateEl: null })}>Apply</Button>
+              <TextField
+                type="date"
+                name="start_date"
+                onChange={this.handleChange}
+                style={{ marginRight: 10 }}
+              />
+              <TextField
+                type="date"
+                name="end_date"
+                onChange={this.handleChange}
+              />
+              <Button
+                onClick={() =>
+                  this.setState({
+                    customDateEl: null,
+                    interval: "1 week",
+                    start_date: "",
+                    end_date: ""
+                  })
+                }
+              >
+                Cancel
+              </Button>
+              <Button
+                disabled={!start_date || !end_date}
+                variant="outlined"
+                onClick={() => this.setState({ customDateEl: null })}
+              >
+                Apply
+              </Button>
             </CardContent>
           </Card>
         </Popper>
--- src/components/StatsTable.js
+++ src/components/StatsTable.js
@@ -1,48 +1,103 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, Fragment } from "react";
 import {
-    Button, Table,
-    TableHead,
-    TableCell,
-    TableBody, TableRow
-} from '@material-ui/core';
+  Button,
+  Table,
+  TableHead,
+  TableCell,
+  TableBody,
+  TableRow
+} from "@material-ui/core";
 import { ImportExport } from "@material-ui/icons";
-import { format } from 'date-fns';
-
+import { format } from "date-fns";
 
 export default class StatsTable extends PureComponent {
-    render() {
-        return (
-            <div className="full-width">
-                <h1>Schedule report</h1>
-                <div className="flex">
-                    <p>This report shows each individual event with Wexer Count data if it is available.</p>
-                    <Button style={{ marginLeft: 'auto' }}>
-                        EXPORT <ImportExport style={{ marginLeft: 15 }} />
-                    </Button>
-                </div>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Title</TableCell>
-                            <TableCell>Date</TableCell>
-                            <TableCell>Start time</TableCell>
-                            <TableCell numeric>Head count</TableCell>
-                            <TableCell>Type</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {this.props.data.map(dataEntry => (
-                            <TableRow>
-                                <TableCell>{dataEntry.name}</TableCell>
-                                <TableCell>{format(dataEntry.date, 'dddd Do MMMM')}</TableCell>
-                                <TableCell>{format(dataEntry.date, 'HH:mm')}</TableCell>
-                                <TableCell numeric>{dataEntry.views}</TableCell>
-                                <TableCell>{dataEntry.type}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </div>
-        )
+  render() {
+    let report = null;
+    if (this.props.report === "schedule_report") {
+      report = (
+        <Fragment>
+          <h1>Schedule report</h1>
+          <div className="flex">
+            <p>
+              This report shows each individual event with Wexer Count data if
+              it is available.
+            </p>
+            <Button style={{ marginLeft: "auto" }}>
+              EXPORT <ImportExport style={{ marginLeft: 15 }} />
+            </Button>
+          </div>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Title</TableCell>
+                <TableCell>Date</TableCell>
+                <TableCell>Start time</TableCell>
+                <TableCell numeric>Head count</TableCell>
+                <TableCell>Type</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.props.data.map(dataEntry => (
+                <TableRow>
+                  <TableCell>{dataEntry.video_title_long}</TableCell>
+                  <TableCell>
+                    {format(dataEntry.datostempel, "dddd Do MMMM")}
+                  </TableCell>
+                  <TableCell>
+                    {format(dataEntry.datostempel, "HH:mm")}
+                  </TableCell>
+                  <TableCell numeric>{dataEntry.views}</TableCell>
+                  <TableCell>
+                    {dataEntry.ondemandselections ? "On-Demand" : "Scheduled"}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Fragment>
+      );
+    } else if (this.props.report === "class_report") {
+      report = (
+        <Fragment>
+          <h1>Class title report </h1>
+          <div className="flex">
+            <p>
+              This report shows the total number of views for each title within
+              the selected timeframe.
+            </p>
+            <Button style={{ marginLeft: "auto" }}>
+              EXPORT <ImportExport style={{ marginLeft: 15 }} />
+            </Button>
+          </div>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Title</TableCell>
+                <TableCell>Category</TableCell>
+                <TableCell>Level</TableCell>
+                <TableCell>Provider</TableCell>
+                <TableCell numeric>Views</TableCell>
+                <TableCell numeric>Count</TableCell>
+                <TableCell numeric>Avg</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.props.data.map(dataEntry => (
+                <TableRow>
+                  <TableCell>{dataEntry.video_title_long}</TableCell>
+                  <TableCell>{dataEntry.video_category}</TableCell>
+                  <TableCell>{dataEntry.video_level}</TableCell>
+                  <TableCell>{dataEntry.providername}</TableCell>
+                  <TableCell numeric>{dataEntry.views}</TableCell>
+                  <TableCell numeric>{dataEntry.count}</TableCell>
+                  <TableCell numeric>{dataEntry.avg}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Fragment>
+      );
     }
+    return <div className="full-width">{report}</div>;
+  }
 }
